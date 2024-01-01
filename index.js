c2df6a4cf92e7aa90cf671b47454737fc8a80702
Abrar <hossainabrar@aol.com>
2024-01-01 17:54:39 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(userName, userEmail){
+    const userDetails = {
+        name: userName,
+        email: userEmail
+    }
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(userDetails)
+    }
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(resp => resp.json())
+    .then(json => appendToDom(json))
+    .catch(error => {
+        document.body.innerHTML += `<p>Error: ${error.message}<p>`
+    })
+
+    function appendToDom(object){
+        const id = object.id
+        document.body.innerHTML += `<p>${id}<p>`
+    }
+}
